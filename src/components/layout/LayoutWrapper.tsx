6a96be2ff323f56fb4a642cf47f85cfbb3270960
Joshua Varon <joshuavaron@gmail.com>
2025-12-25 20:18:46 -0600

    donate bug

--- src/components/layout/LayoutWrapper.tsx
+++ src/components/layout/LayoutWrapper.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 // Pages that should not show header/footer (immersive/trapped pages)
-const HEADERLESS_PAGES = ["/donate"];
+const HEADERLESS_PAGES = ["/donate", "/es/donate"];
 
 interface LayoutWrapperProps {
   header: ReactNode;
